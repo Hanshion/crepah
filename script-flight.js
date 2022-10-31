cb5249908bb904c3a36d8bc34b79ece5b147c248
Hanshion <hanshion8@gmail.com>
2022-10-31 23:00:18 +0300

    Update script-flight.js

--- script-flight.js
+++ script-flight.js
@@ -1,10 +1,33 @@
-const list = document.querySelector('list'),
-items = document.querySelectorAll('.flight')
+const list = document.querySelector('.list')
+const items = document.querySelectorAll('.flight')
 
 function filter() {
     list.addEventListener('click', event => {
         const targetId = event.target.dataset.id
         console.log(targetId)
+
+        switch(targetId) {
+            case 'all':
+            break
+            case 'local':
+                items.forEach(item => {
+                    if(item.classList.contains('local')){
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                })
+            break
+            case 'international':
+                items.forEach(item => {
+                    if(item.classList.contains('international')){
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                })
+            break
+        }
     })
 }
 filter()
