fd8c26b03ed3ebee2335c24bb7ba453479d3bc58
Dzhentelmen <inspectorboink@yandex.ru>
2022-11-07 17:07:49 +0300

    le fix of le fix

--- scripts/signup.php
+++ scripts/signup.php
@@ -1,15 +1,13 @@
 <?php
 session_start();
 require_once 'connect.php';
-// $connect = mysqli_connect('localhost', 'root', null, 'RoyalAirlines');
 
 $login = $_POST['login'];
 $password = $_POST['password'];
 $password_confirm = $_POST['password_confirm'];
 
 if ($password === $password_confirm){
-    if (mysqli_query($connect, "INSERT INTO `Users` (`login`, `password`) VALUES ('$login', '$password')")){
-    mysqli_query($connect, "INSERT INTO `Users` (`login`, `password`) VALUES ($login, $password)");
+    mysqli_query($connect, "INSERT INTO `Users` (`login`, `password`) VALUES ('$login', '$password')");
     $_SESSION['message'] = 'Регистрация прошла успешно!';
     header('location: ../login.php');
 } else {
